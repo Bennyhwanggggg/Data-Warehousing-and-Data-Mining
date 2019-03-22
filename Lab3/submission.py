a37179a2e601f4d2966469903036c26d62f75af4
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-03-22 14:50:42 +1100

    lab3 done

--- Lab3/submission.py
+++ Lab3/submission.py
@@ -1,2 +1,13 @@
+import numpy as np
+
 def logistic_regression(data, labels, weights, num_epochs, learning_rate): # do not change the heading of the function
-    pass # **replace** this line with your code
+	data = np.hstack((np.ones((data.shape[0], 1)), data))
+	for epoch in range(num_epochs):
+		p = 1/(1+np.exp(-1*np.dot(data, weights)))
+		errors = labels-p
+		sse = np.sum(np.square(errors))
+		gradient = np.dot(errors, data)
+		weights = weights + learning_rate*gradient
+	return weights
+
+
